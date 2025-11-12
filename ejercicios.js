4fc7d83455fbb177629d59a03f845fa23cb66598
Luis Gerardo Sanchez Mendoza <nova@MacBook-Pro-de-Luis.local>
2025-11-11 20:09:26 -0600

    Ejercicio #4.3 completado

--- ejercicios.js
+++ ejercicios.js
@@ -246,8 +246,9 @@ function promedioArray(numeros) {
  */
 function encontrarMaximo(numeros) {
   // TODO: Encuentra y retorna el nÃºmero mÃ¡s grande
-  // Pista: Puedes usar Math.max(...numeros) o hacerlo con un bucle
-  return 0;
+  // Pista: Puedes usar Math.max(...numeros) o hacerlo con un bucle\
+    if (numeros.length === 0) return 0;
+  return Math.max(...numeros);
 }
 
 /**
